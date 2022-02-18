df5eb11c195beb73b7951db5380ebe8c84374b4d
TAHAWE <amer.zp582@gmail.com>
2022-02-19 00:04:42 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+// Collecting Data
+
+const slideImage = document.querySelectorAll(".slide-image");
+
+const slidesContainer = document.querySelector(".slides-container");
+
+const numberOfImages = slideImage.length;
+
+const navigationDots = document.querySelector(".navigation-dots");
+
+const nextBtn = document.querySelector(".next");
+const prevBtn = document.querySelector(".previous");
+
+// Set up the slider
+//
+function init() {
+  slideImage.forEach((img, i) => {
+    img.style.left = i * 100 + "%";
+  });
+  slideImage[0].classList.add("active");
+  createNavigationDots();
+}
+init();
+
+function createNavigationDots() {
+  for (let i = 0; i < numberOfImages; i++) {
+    const dot = document.createElement("li");
+    navigationDots.appendChild(dot);
+  }
+}
